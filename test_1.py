dc931595586f925cd727065562cfaf08792635e0
Por Xiong <porxiong2020@outlook.com>
2022-03-25 13:52:55 -0700

    finish exam

--- test_1.py
+++ test_1.py
@@ -38,3 +38,29 @@ else
 
 run main function
 """
+
+def option1():
+    return 100 * 10
+
+def option2():
+    amount = 1
+    list1 = []
+    for i in range(0, 10):
+        list1.append(amount)
+        amount *= 2 
+    total = sum(list1)
+    return total
+
+def main(): 
+    answer = ""
+    var1 = option1()
+    var2 = option2()
+    if var1 == var2:
+        answer = "Option 1 and Option 2 pays the same"
+    if var1 > var2:
+        answer = "Option 1 is better"
+    else:
+        answer = "Option 2 is better"
+    print(answer)
+
+main()
